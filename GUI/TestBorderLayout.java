3dc15e0d199c64254db099dd81e44cd487bffca8
Liu_Jin <389594458@qq.com>
2018-11-22 22:39:15 +0800

    add Layout test

---
+++ GUI/TestBorderLayout.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class TestBorderLayout {
+    public static void main(String args[]) {
+        Frame f = new Frame("BorderLayout Example");
+        
+        Button bn = new Button("BN");
+        Button bs = new Button("BS");
+        Button bw = new Button("BW");
+        Button be = new Button("BE");
+        Button bc = new Button("BC");
+        
+        f.add(bn, BorderLayout.NORTH);
+        f.add(bs, BorderLayout.SOUTH);
+        f.add(bw, BorderLayout.WEST);
+        f.add(be, BorderLayout.EAST);
+        f.add(bc, BorderLayout.CENTER);
+        
+        /*
+        f.add(bn, "North");
+        f.add(bs, "South");
+        f.add(bw, "West");
+        f.add(be, "East");
+        f.add(bc, "Center");
+        */
+        
+        f.setSize(200, 200);
+        f.setVisible(true);
+    }
+}
---
+++ GUI/TestFlowLayout2.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class TestFlowLayout2 {
+    public static void main(String args[]) {
+        Frame f = new Frame("Java Frame");
+        FlowLayout l =
+            new FlowLayout(FlowLayout.CENTER, 20, 40);
+        f.setLayout(l);
+        f.setLocation(100,100);
+        f.setSize(300,200);
+        f.setBackground(new Color(204, 204, 255));
+        for(int i=1; i<=7; i++) {
+            f.add(new Button("button"));
+        }
+        f.setVisible(true);
+    }
+}
